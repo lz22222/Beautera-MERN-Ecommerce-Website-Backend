e26a670174c041b5f20f7e42083919e636cc4d5f
“Ling <lzhuo2@ualberta.ca>
2024-12-01 15:05:00 -0700

    update url

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://beautera-mern-ecommerce-website-frontend.vercel.app/',   //http://localhost:5173
     credentials: true,
   })
 );
--- src/orders/orders.route.js
+++ src/orders/orders.route.js
@@ -28,8 +28,8 @@ const lineItems = products.map((product) => {
       payment_method_types: ["card"],
       line_items: lineItems,
       mode: "payment",
-      success_url:"http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}",
-			cancel_url: "http://localhost:5173/cancel",
+      success_url:"https://beautera-mern-ecommerce-website-frontend.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",//http://localhost:5173/success?session_id={CHECKOUT_SESSION_ID}
+			cancel_url: "https://beautera-mern-ecommerce-website-frontend.vercel.app/cancel", //http://localhost:5173/cancel
     });
     res.json({ id: session.id });
   } catch (error) {
